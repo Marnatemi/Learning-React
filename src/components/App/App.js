e9c6f39f5de70544419fe4bbf2772ff6090f8fbe
Marlena Czerwińska <marlena.stef@gmail.com>
2019-10-11 03:43:33 +0200

    Add List.js and Hero.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,6 @@
   
 import React from 'react';
-
+import List from '../List/List.js';
 import styles from './App.scss';
 
 class App extends React.Component {
@@ -9,6 +9,7 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>My first React app</h1>
         <h2 className={styles.subtitle}>Hello World!</h2>
+        <List />
       </main>
     )
   }
