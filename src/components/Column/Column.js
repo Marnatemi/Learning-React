b1d7b5d2be7967084ed7206f90ebfec5a9b5f85a
Marlena Czerwińska <marlena.stef@gmail.com>
2019-10-12 03:37:30 +0200

    Add Icon.js and Change Column.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styles from './Column.scss';
 import Card from '../Card/Card.js';
 import Creator from '../Creator/Creator.js';
+import Icon from '../Icon/Icon.js'
 import {settings} from '../../data/dataStore.js';
 
 class Column extends React.Component {
@@ -12,6 +13,7 @@ class Column extends React.Component {
 
   static propTypes = {
     title: PropTypes.string.isRequired,
+    icon: PropTypes.string,
   }
 
   addCard(title){
@@ -31,13 +33,13 @@ class Column extends React.Component {
   render() {
     return(
       <section className={styles.component}>
-        <h3 className={styles.title}>{this.props.title}</h3>
+        <h3 className={styles.title}>{this.props.title}<span className={styles.icon}><Icon name={this.props.icon} /></span></h3>
         <div >
           {this.state.cards.map(({key, ...cardProps}) => (
             <Card key={key} {...cardProps} />
           ))}
         </div>
-        <div className={styles.creator}>
+        <div>
           <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
         </div>
       </section>
